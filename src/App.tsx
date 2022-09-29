c6799222767f381354fafdb2aaaabc6917bb73ed
filipcondac <filipcondac@gmail.com>
2022-09-29 13:14:43 +0300

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <main className="flex flex-col h-screen justify-center">
       <h1 className="text-5xl m-auto">
-        React + Tailwind + Typescript + Vite Template
+        React + Tailwind + Typescript + Vite Template 🧨
       </h1>
     </main>
   );
